fc4d4ae91e7ae8c304eb919516feab443b971d55
Andrey Blazejuk <ablazejuk@inf.ufrgs.br>
2014-11-15 18:10:31 -0200

    Implementação Conference, Paper e PaperGrade
    Implementação das classes e seus respectivos testes

--- ConferenceManager/src/conference/manager/business/domain/PaperGradeTest.java
+++ ConferenceManager/src/conference/manager/business/domain/PaperGradeTest.java
@@ -2,13 +2,47 @@ package conference.manager.business.domain;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PaperGradeTest {
 
+	PaperGrade grade;
+	Reviewer reviewer;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		reviewer = new Reviewer();
+	}
+	
+	/**
+	 * Test method for {@link conference.manager.business.domain.PaperGrade
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testValidLowerGrade() {
+		grade = new PaperGrade(reviewer, -3);
+		assertTrue(grade.isValidGrade());
+	}
+	
+	@Test
+	public void testInvalidLowerGrade() {
+		grade = new PaperGrade(reviewer, -4);
+		assertFalse(grade.isValidGrade());
+	}
+	
+	@Test
+	public void testValidHigherGrade() {
+		grade = new PaperGrade(reviewer, 3);
+		assertTrue(grade.isValidGrade());
+	}
+	
+	@Test
+	public void testInvalidHigherGrade() {
+		grade = new PaperGrade(reviewer, 4);
+		assertFalse(grade.isValidGrade());
 	}
 
 }
