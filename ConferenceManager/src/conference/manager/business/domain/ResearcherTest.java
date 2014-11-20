26dd69c5f520ed1fa219ade34f6d8db5f5e47df6
Leonardo Hahn <leonardohahn@outlook.com>
2014-11-20 20:24:19 -0200

    Adicionado ResearcherTest

--- ConferenceManager/src/conference/manager/business/domain/ResearcherTest.java
+++ ConferenceManager/src/conference/manager/business/domain/ResearcherTest.java
@@ -2,13 +2,30 @@ package conference.manager.business.domain;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class ResearcherTest {
+import conference.manager.data.Database;
 
+public class ResearcherTest {
+	
+	Database database;
+	
+	@Before
+	public void setUp() {
+		database = new Database();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEquals() {
+		List<Researcher> researchers = database.getResearchers();
+		Researcher r1 = researchers.get(0);
+		Researcher r2 = researchers.get(1);
+		
+		assertFalse(r1.equals(r2));
+		assertTrue(r1.equals(r1));
 	}
 
 }
