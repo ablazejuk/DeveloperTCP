66d1cfde285a684265808d05591f92859be64b6a
Andrey Blazejuk <ablazejuk@inf.ufrgs.br>
2014-11-20 23:38:30 -0200

    Pré-Implementação do teste para GradeAssignment
    Teste precisa que o primeiro artigo da primeira conferência esteja
    alocado.

--- ConferenceManager/src/conference/manager/business/impl/GradeAssignmentServiceImplTest.java
+++ ConferenceManager/src/conference/manager/business/impl/GradeAssignmentServiceImplTest.java
@@ -2,13 +2,37 @@ package conference.manager.business.impl;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import conference.manager.business.domain.Conference;
+import conference.manager.business.domain.Paper;
+import conference.manager.business.domain.Reviewer;
+import conference.manager.data.Database;
 
 public class GradeAssignmentServiceImplTest {
 
+	GradeAssignmentServiceImpl gradeAssignmentServiceImpl;
+	Database database;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		database = new Database();
+		gradeAssignmentServiceImpl = new GradeAssignmentServiceImpl(database);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAssignGrade() {
+		//chamar método de alocação de conferências aqui!
+		Conference c = database.geAllocatedConferences().get(0);
+		Paper paper = c.getUngradedPapers().get(0);
+		Reviewer reviewer = paper.getReviewers().get(0);
+
+		gradeAssignmentServiceImpl.assignGrade(paper, reviewer, 0);
+		assertTrue(paper.isGraded());
 	}
 
 }
